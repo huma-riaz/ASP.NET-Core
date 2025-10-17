126df20a7456e5546f3925ae89d36e6189580a57
Huma Riaz <riazhuma2005@gmail.com>
2025-10-17 22:32:23 +0500

    Edited

---
+++ FirstProject/FirstProject/Program.cs
@@ -0,0 +1,29 @@
+namespace FirstProject
+{
+    public class program
+    {
+        public static void Main(string[] args)
+        {
+
+         var builder = WebApplication.CreateBuilder(args);
+            builder.Services.AddMvc();
+         var app = builder.Build();
+
+            // Simple 
+            //app.MapGet("/", () => "Hello World!");
+
+            // For Only Controller 
+            //app.MapControllers();
+
+            // With Views
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}"
+                );
+
+
+         app.Run();
+
+        }
+    }
+}
---
+++ FirstProject/FirstProject/controller/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FirstProject.Controllers 
+{
+    public class HomeController : Controller 
+    {
+
+
+
+
+        // Start With Views 
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+
+     /* Only with Controller
+      
+    [Route("Home/Index")]
+        [Route("~/")]
+        public string Index()
+        {
+            return "Index Page";
+        }
+
+        [Route("Home/About")]
+        public string About()
+        {
+            return "About Page";
+        }
+
+        [Route("Home/Details/{name}")]
+        public string Details(string name)
+        {
+            return name;
+        }
+*/
+
+
+    }
+}
