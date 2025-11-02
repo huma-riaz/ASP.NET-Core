47744ced9f3a7aa23854e80dcf2441e243367dda
Huma Riaz <riazhuma2005@gmail.com>
2025-11-02 13:45:22 +0500

    Edited

---
+++ CRUD/CRUD/Controllers/ProductController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CRUD.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+    }
+}
--- CRUD/CRUD/obj/Debug/net8.0/CRUD.AssemblyInfo.cs
+++ CRUD/CRUD/obj/Debug/net8.0/CRUD.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CRUD")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ea9ddc117dae8ad96ff878ff8670c6b1b0dd10c8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+861c6ddd54323f389cdb344dcd8335b9f51c764e")]
 [assembly: System.Reflection.AssemblyProductAttribute("CRUD")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CRUD")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
