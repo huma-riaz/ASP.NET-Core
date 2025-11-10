bd177112443eeea1b369c4b48403568207e28195
Huma Riaz <riazhuma2005@gmail.com>
2025-11-10 22:27:57 +0500

    Edited

--- TemplateImplement/TemplateImplement/Controllers/ProductController.cs
+++ TemplateImplement/TemplateImplement/Controllers/ProductController.cs
@@ -1,12 +1,20 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using TemplateImplement.Models;
 
 namespace TemplateImplement.Controllers
 {
     public class ProductController : Controller
     {
+        private readonly TemplateContext _context;
+        public ProductController(TemplateContext mycontext)
+        {
+            this._context = mycontext;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            var products = _context.tbl_products.ToList();
+            return View(products);
         }
         public IActionResult Add()
         {
--- TemplateImplement/TemplateImplement/obj/Debug/net8.0/TemplateImplement.AssemblyInfo.cs
+++ TemplateImplement/TemplateImplement/obj/Debug/net8.0/TemplateImplement.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TemplateImplement")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+31be0f0251f6b640a64b676435194d63f92e88e1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dd9304add7f663995d2046b99d57018c640ab307")]
 [assembly: System.Reflection.AssemblyProductAttribute("TemplateImplement")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TemplateImplement")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
