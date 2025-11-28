85cfa6c42f3aa3f9b96babdcfb39462f1b0cfd2a
Huma Riaz <riazhuma2005@gmail.com>
2025-11-28 21:14:16 +0500

    Edited

---
+++ Talent Showcase Project/Talent Showcase Project/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Talent_Showcase_Project.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
